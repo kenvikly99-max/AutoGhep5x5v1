39f6476dcd9d15ce053fab6c76472e0c88eac5ef
kenvikly99-max <kenvikly99@gmail.com>
2026-08-18 19:45:17 +0700

    Create MainActivity.kt

---
+++ app/src/main/app/src/main/java/com/example/autoghep5x5/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.autoghep5x5
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION, 
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 1234)
+        } else {
+            startService(Intent(this, FloatingButtonService::class.java))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1234) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                startService(Intent(this, FloatingButtonService::class.java))
+            }
+        }
+    }
+
+    companion object {
+        fun compareBitmaps(bmp1: Bitmap, bmp2: Bitmap): Double {
+            var matchingPixels = 0
+            val totalPixels = bmp1.width * bmp1.height
+            val step = 4
+
+            for (x in 0 until bmp1.width step step) {
+                for (y in 0 until bmp1.height step step) {
+                    val p1 = bmp1.getPixel(x, y)
+                    val p2 = bmp2.getPixel(x, y)
+
+                    val rDiff = abs((p1 shr 16 and 0xff) - (p2 shr 16 and 0xff))
+                    val gDiff = abs((p1 shr 8 and 0xff) - (p2 shr 8 and 0xff))
+                    val bDiff = abs((p1 and 0xff) - (p2 and 0xff))
+
+                    if (rDiff + gDiff + bDiff < 45) {
+                        matchingPixels++
+                    }
+                }
+            }
+            return matchingPixels.toDouble() / (totalPixels / (step * step))
+        }
+    }
+}
